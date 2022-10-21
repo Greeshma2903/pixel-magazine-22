b5181bfec15230beef56fa9e20ac09f8fe0af187
Greeshma Medam <greeshma.medam@gmail.com>
2022-10-21 21:39:44 +0530

    Testing webp images 😅

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-// import Cover from '/src/assets/cover.png'
-// import Contents from '/src/assets/contents.png'
-// import img1 from '/src/assets/1.png'
 
 // Import Swiper styles
 import "swiper/css";
@@ -12,7 +9,7 @@ import "swiper/css/pagination";
 import "swiper/css/lazy";
 
 // import required modules
-import {Lazy, Navigation, Pagination, Keyboard } from "swiper";
+import { Lazy, Navigation, Pagination, Keyboard } from "swiper";
 
 export default function App() {
   const pagination = {
@@ -49,25 +46,21 @@ export default function App() {
         className="mySwiper"
       >
         <SwiperSlide>
-          <img src={"/cover.png"} className="swiper-lazy" alt="Cover page of magazine"/>
+          <img
+            src={"/cover.webp"}
+            className="swiper-lazy"
+            alt="Cover page of magazine"
+          />
           <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={"/contents.png"} className="swiper-lazy" alt="Table of Contents"/>
-          <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={"/1.png"} className="swiper-lazy" alt=" "/>
-          <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={"/2.png"} className="swiper-lazy" alt=" "/>
-          <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={"/3.png"} className="swiper-lazy" alt=" "/>
-          <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-        </SwiperSlide>
+        {Array.from(Array(25).keys(), (n) => n + 1).map((count) => {
+          return (
+            <SwiperSlide>
+              <img src={`/${count}.webp`} className="swiper-lazy" alt=" " />
+              <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
       <footer>
         made with 🤟 by
